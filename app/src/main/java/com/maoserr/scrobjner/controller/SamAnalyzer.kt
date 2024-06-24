0ee3244cbb77495a520231ccacb33b6b0687cb97
Maoserr <maoserr@gmail.com>
2024-06-23 19:22:58 -0500

    fix size

--- app/src/main/java/com/maoserr/scrobjner/controller/SamAnalyzer.kt
+++ app/src/main/java/com/maoserr/scrobjner/controller/SamAnalyzer.kt
@@ -6,19 +6,20 @@ import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.compose.runtime.MutableState
+import com.maoserr.scrobjner.utils.resizeSrc
 
 internal class FrameProc(
     private val run: MutableState<Boolean>,
     private val outimg: MutableState<Bitmap?>,
     private val onCap: (() -> Unit)?
-): ImageAnalysis.Analyzer {
+) : ImageAnalysis.Analyzer {
 
     @OptIn(ExperimentalGetImage::class)
     override fun analyze(image: ImageProxy) {
         if (run.value) {
             run.value = false
             image.image?.let {
-                outimg.value = image.toBitmap()
+                outimg.value = resizeSrc(image.toBitmap())
                 onCap?.invoke()
             }
         }
